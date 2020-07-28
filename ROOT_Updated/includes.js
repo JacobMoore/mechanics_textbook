c27ebf49d77a719ec506e01a85b6f7409d4bb9e1
Jacob Moore <JacobPMoore@gmail.com>
2020-07-28 13:48:57 -0400

    Fix to unit conversion table

--- ROOT_Updated/includes.js
+++ ROOT_Updated/includes.js
@@ -35,7 +35,7 @@ function displayMenuSub(){
 function displayResourcesMain(){
 	document.write('<h2>Useful Resources</h2>');
 	document.write('<div class="resource">');
-	document.write('<a href="websites/Statics and Dynamics Units and Conversion Table.pdf" target="_blank">Units and Conversion Table</a>');
+	document.write('<a href="http://mechanicsmap.psu.edu/websites/UnitConversion.pdf" target="_blank">Units and Conversion Table</a>');
 	document.write('</div>');
 	document.write('<div class="resource">');
 	document.write('<a href="http://www.wolframalpha.com/examples/EquationSolving.html" target="_blank">Wolfram Equation Solver</a>');
