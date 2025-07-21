acd84b425c9a83ccdf86834a424784e7fd7df591
Jacob Moore <JacobPMoore@gmail.com>
2025-07-21 15:31:44 -0400

    Cert Update

--- ROOT_Updated/includes.js
+++ ROOT_Updated/includes.js
@@ -47,10 +47,10 @@ function displayResources(){
 	document.write('<a href="https://www.wolframalpha.com/examples/mathematics/linear-algebra/vectors" target="_blank">Wolfram Alpha Vector Operation Calculator</a>');
 	document.write('</div>');
 	document.write('<div class="resource">');
-	document.write('<a href="http://mechanicsmap.psu.edu/websites/centroidtables/centroids2D/centroids2D.html" target="_blank">2D Centroid Location and Area Moment of Inertia Table</a>');
+	document.write('<a href="http://mechanicsmap.psu.edu/websites/centroidtables/centroids2D/centroids2D.html" target="_blank">2D Centroid and Area Moment of Inertia Table</a>');
 	document.write('</div>');
 	document.write('<div class="resource">');
-	document.write('<a href="http://mechanicsmap.psu.edu/websites/centroidtables/centroids3D/centroids3D.html" target="_blank">3D Centroid Location and Mass Moment of Inertia Table</a>');
+	document.write('<a href="http://mechanicsmap.psu.edu/websites/centroidtables/centroids3D/centroids3D.html" target="_blank">3D Centroid and Mass Moment of Inertia Table</a>');
 	document.write('</div>');
 }	
 
